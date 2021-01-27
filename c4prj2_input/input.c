ef53cbe442d0b59475a31c36d94d65d5107ba869
Coursera Learner <nobody@nowhere.nul>
2021-01-27 15:32:38 +0000

    done

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -5,6 +5,40 @@
 #include "deck.h"
 #include "cards.h"
 #include "future.h"
+
+
+
+
+deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
+  assert(f!=NULL);
+  size_t num = 0;
+  size_t sz = 0;
+  char * line = NULL;
+  deck_t ** r_deck = NULL;
+  while(getline(&line, &sz, f)!=EOF){
+
+    // create a hand from each valid line in file
+    r_deck=realloc(r_deck,sizeof(* r_deck)*(num+1));
+    deck_t * temp = hand_from_string(line,fc);
+    if (temp!=NULL){
+      // import "temp" to returned deck
+      r_deck[num]=temp;
+      num++;
+    }else{
+      // if line invalid go to next line or end if EOF
+      continue;
+    }
+
+
+  }
+  * n_hands = num;
+  free(line);
+  return r_deck;
+}
+
+
+
+
 deck_t * hand_from_string(const char * str, future_cards_t * fc){
   deck_t* deck=malloc(sizeof(*deck));
   deck->cards = NULL;
@@ -34,20 +68,7 @@ deck_t * hand_from_string(const char * str, future_cards_t * fc){
     fprintf(stderr,"asas");}
   return deck;
 }
-deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
-  deck_t **arr=NULL;
-  size_t n_hand=0;
-  char *line=NULL;
-  size_t sz=0;
-  while(getline(&line,&sz,f)>=0){
-    arr=realloc(arr,(n_hand+1)*sizeof(*arr));
-    deck_t*deck=hand_from_string(line,fc);
-    if (deck == NULL)continue;
-    arr[n_hand]=deck;
-    n_hand ++;
-  }
-  free(line);
-  *n_hands=n_hand;
-  return arr;
-}
+
+
+
 
