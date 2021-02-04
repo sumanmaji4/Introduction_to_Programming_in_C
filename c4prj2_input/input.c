1146811dc78cb0d048dd074510a6c43052c87f62
Coursera Learner <nobody@nowhere.nul>
2021-02-04 13:48:11 +0000

    whats wrong

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -9,9 +9,50 @@
 
 
 
-deck_t * hand_from_string(const char * str, future_cards_t * fc){ }
 
-  
+deck_t * hand_from_string(const char * str, future_cards_t * fc){
+  deck_t * deck=malloc(sizeof(*deck));
+  deck->cards=NULL;
+  deck->n_cards=0;
+
+  // analyse imported line
+  for (int i=0; i<strlen(str); i++){
+
+    // ignore characters space or newline
+    if(str[i] == ' ' || str[i] == '\n') continue;
+
+    // if card is not unknown, add it
+    if(str[i] != '?') {
+      card_t card = card_from_letters(str[i], str[i+1]);
+      add_card_to(deck,card);
+      i++;
+    }else{
+      // if card is unknown add a future card
+      char str_n[strlen(str)];
+      int n=0;
+      i++;
+
+      // copy all card characters to the string up to next newline or space
+      while (!(str[i] == ' '|| str[i] == '\n' )){
+	str_n[n]=str[i];
+	i++;
+	n++;
+      }
+      // finish card string with terminator
+      str_n[n]= '\0';
+      add_future_card(fc, atoi(str_n), add_empty_card(deck));
+    }
+  }
+
+  // hands have to be minimum 5 cards
+  if (deck->n_cards < 5) {
+    fprintf(stderr, "Hands smaller than 5 cards");
+    return NULL;
+  }
+  return deck;
+
+}
+
 
 deck_t ** read_input(FILE * f, size_t * n_hands, future_cards_t * fc){
   assert(f!=NULL);
