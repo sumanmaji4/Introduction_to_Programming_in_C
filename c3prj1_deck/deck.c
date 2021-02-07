a9f5bed48864e309adf2a74f4e0649cde4e0a7ab
Coursera Learner <nobody@nowhere.nul>
2021-02-07 09:54:28 +0000

    done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -2,52 +2,64 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "deck.h"
-void print_hand(deck_t * hand){
-  card_t **card = hand->cards;
-  card_t temp;
-  for(int i=0; i<(hand->n_cards); i++){
-    temp = **card;
-    print_card(temp);
-    printf(" ");
-    card++;
-  }
-}
 
-int deck_contains(deck_t * d, card_t c) {
-  card_t **card;
-  for(int i=0; i<(d->n_cards); i++){
-    card = d->cards+i;
-    if((value_letter(**card) == value_letter(c)) && (suit_letter(**card) == suit_letter(c)))
+
+
+
+
+int deck_contains(deck_t * d, card_t c){
+
+  for (size_t i=0; i<(d->n_cards); i++) {
+    if (((d->cards[i]->value) == c.value) && ((d->cards[i]->suit)==c.suit)) {
+      //  printf("contained in deck");
       return 1;
-    card++;
+    }
   }
   return 0;
 }
 
-void shuffle(deck_t * d){
-  card_t *temp, **card = d->cards;
-  size_t limit = d->n_cards;
-  int a;
-  for(size_t i=0; i<limit/2; i++){
-    a = random()%(limit-i)+i;
-    temp = card[i];
-    card[i] = card[a];
-    card[a] = temp;
+void print_hand(deck_t * hand){
+  for (size_t i=0; i<(hand->n_cards); i++) {
+    print_card(*(hand->cards[i]));
+    printf(" ");
   }
+
+  // for (size_t i=0; i< n_cards; i++){
+  //print_card(*(hand).card_t[i]);
+  //printf(" ");
+  //}
+}
+
+void shuffle(deck_t * d){
+  size_t max_rand=d->n_cards;
+  card_t swap_card;
+  size_t num;
+  size_t num2;
+  for (size_t i=0; i<max_rand; i++){
+    num=rand()%max_rand;
+    num2=rand()%max_rand;
+    //   while (num>max_rand){
+    //num/=2;
+    //}
+    // printf("num =%zu ", num);
+    swap_card.value=d->cards[num]->value;
+    swap_card.suit=d->cards[num]->suit;
+    d->cards[num]->value=d->cards[num2]->value;
+    d->cards[num]->suit=d->cards[num2]->suit;
+    d->cards[num2]->value=swap_card.value;
+    d->cards[num2]->suit=swap_card.suit;
+  }
+
+
 }
 
 void assert_full_deck(deck_t * d) {
-  card_t **card = d->cards;
-  card_t temp_card;
-  deck_t deck;
-  deck.cards = d->cards;
-  for(int i=0; i<(d->n_cards); i++){
-    temp_card = **card;
-    assert_card_valid(temp_card);
-    if(i>0){
-      deck.n_cards = (size_t)i;
-      assert(!deck_contains(&deck, temp_card));
-    }
-    card++;
+  // printf("running assert_full_deck");
+  assert(d->n_cards == 52);
+  for (size_t i=0; i < d->n_cards; i++){
+    assert_card_valid(*(d->cards[i]));
+    assert(deck_contains(d, card_from_num(i))==1);
   }
+
 }
+
